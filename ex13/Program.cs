f73d65c5c3c6cda80c25923b764d74a04ce36695
Mari <matorina.mariya@mail.ru>
2023-06-18 17:36:25 +0300

    ex10,ex15

---
+++ ex13/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int num3 = num%1000/100;
+if (num3 == 0) Console.WriteLine("третьей цифры нет");
+else Console.WriteLine(num3);
